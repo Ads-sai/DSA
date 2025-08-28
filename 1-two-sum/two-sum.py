2c95b15c24d6de190871c282416808d6de76ecce
Ads-sai <147010372+Ads-sai@users.noreply.github.com>
2025-08-28 19:50:32 +0530

    Time: 2029 ms (39.60%) | Memory: 13.3 MB (31.28%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,6 +5,4 @@ class Solution(object):
         for i in range(n):
             for j in range(n-1,i,-1):
                 if nums[i]+nums[j]==target:
-                    a.append(i)
-                    a.append(j)
-        return a
+                    return [i,j]
